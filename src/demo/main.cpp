ceeb8a0a245198290ae25a037190b0aefa05de23
NeacsuRadu <radu-stefan.neacsu@my.fmi.unibuc.ro>
2018-12-10 20:53:51 +0200

    added dependencies(glfw, glew), created basic opengl application

--- src/demo/main.cpp
+++ src/demo/main.cpp
@@ -1,7 +1,62 @@
+#include <GLEW/glew.h>
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 
-int main()
+void mouse_button_callback(GLFWwindow * window, int button, int action, int mods)
 {
-    std::cout << "Hello, world!\n";
+    std::cout << "-- Mouse button callback --" << std::endl;
+    if (button == GLFW_MOUSE_BUTTON_LEFT)
+    {
+        if (action == GLFW_PRESS)
+        {
+            double xpos, ypos;
+            glfwGetCursorPos(window, &xpos, &ypos);
+            std::cout << "-- left button pressed, pos: " << xpos << " " << ypos << " --" << std::endl;
+        }
+    }
+}
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK)
+        std::cout << "Error glew init" << std::endl;
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    glfwSetMouseButtonCallback(window, mouse_button_callback);
+   
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
